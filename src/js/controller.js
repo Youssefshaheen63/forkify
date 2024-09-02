e4822e4cc6182e5b597f413f8f040f4441240d69
Youssef Shaheen <shaheeny616@gmail.com>
2024-09-02 18:35:54 +0300

    Listening For load and hashchange Events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,10 +30,12 @@ const renderSppiner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     renderSppiner(recipeContainer);
     // Loading recipe
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -160,4 +162,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
