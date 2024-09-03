2a274aecb08d6b177c2e86c1ca770b1be4df9790
Youssef Shaheen <shaheeny616@gmail.com>
2024-09-03 15:29:21 +0300

    Event Handlers in MVC Publisher-Subscriber Pattern

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -28,6 +28,10 @@ class RecipeView {
     this.#ParentElement.innerHTML = '';
     this.#ParentElement.insertAdjacentHTML('afterbegin', markup);
   };
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
   #generateMarkup() {
     return `
         <figure class="recipe__fig">
